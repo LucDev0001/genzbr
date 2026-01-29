a0b20b1bc0ff528f274bb99336f227cddce11f11
LucDev0001 <156922180+LucDev0001@users.noreply.github.com>
2026-01-29 01:11:56 -0300

    commit

---
+++ genz/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOFkYl6G3vYo2g1Te4ngSkQiURsII4TZk",
+  authDomain: "genz-3a9a0.firebaseapp.com",
+  projectId: "genz-3a9a0",
+  storageBucket: "genz-3a9a0.firebasestorage.app",
+  messagingSenderId: "728013112963",
+  appId: "1:728013112963:web:698404860065736bf6fdb6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
